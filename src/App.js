dccd266a51177e08dacebffad85f4b44e0604dfc
rickchang <rickchang@rickchangs-MacBook-Pro.local>
2021-03-24 00:40:18 +0800

    chore()

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GoStation Map
         </a>
       </header>
     </div>
