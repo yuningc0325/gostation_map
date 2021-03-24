25f0ff33f01dd13179b398ee7c3ca71d31df9171
rickchang <rickchang@rickchangs-MacBook-Pro.local>
2021-03-25 01:36:38 +0800

    feat() import map and data mark

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import TaiwanMap from './container/TaiwanMap'
+import { Card } from 'antd'
 import './App.css';
+
+const CardStyle = {
+  width: 300,
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  zIndex: 999
+}
 
 function App() {
   return (
     <div className="App">
+      <Card style={CardStyle}>
+        <p>Big Number, Select Count, Percentage</p>
+      </Card>
       <TaiwanMap/>
     </div>
   );
--- src/container/TaiwanMap.jsx
+++ src/container/TaiwanMap.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl'
 import { getSecondItemFromArray } from '../utils'
 import axios from 'axios'
 
@@ -10,6 +11,13 @@ const stateMapping = {
 }
 
 const TaiwanMap = () => {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 23.6978,
+    longitude: 120.9605,
+    zoom: 8
+  })
   const [stationData, setStationData] = useState([])
   useEffect(() => {
     fetchData()
@@ -20,7 +28,6 @@ const TaiwanMap = () => {
     try {
       const res = await axios.get('https://webapi.gogoro.com/api/vm/list')
       const { data } = res
-      console.log('***', data)
       result = parseStationData(data)
     } catch (err) {
       console.log(err)
@@ -45,15 +52,34 @@ const TaiwanMap = () => {
         city,
         district,
         locName,
+        stateCode: dt.State,
         state: stateMapping[dt.State]
       }
-    })
+    }).filter(dt => dt.state === '啟用中')
   }
 
   
 
   return (
-    <div>{stationData.map(o => (<div>{o.locName}</div>))}</div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken='pk.eyJ1IjoieXVuaW5nYzAzMjUiLCJhIjoiY2ttbm96MGs0MDQ5NzJubWdrNnZkM2pvcyJ9.m5pyg4oP7RiLkDkOLYJW0w'
+      // mapStyle='mapbox://styles/yuningc0325/ckmnpe5yalnt817tfwbuf5yeu?optimize=true'
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    >
+      {stationData.length > 0 ? stationData.map(station => (
+        <Marker
+          key={station.rid}
+          latitude={station.latitude}
+          longitude={station.longitude}
+        >
+          <img style={{zIndex: 999, width:10, height:10}} src='station.svg'/>
+        </Marker>
+      )): <></>}
+
+    </ReactMapGL>  
+      
+    // <div>{stationData.map(o => (<div>{o.locName}</div>))}</div>
   )
 }
 
