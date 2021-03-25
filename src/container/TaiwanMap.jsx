41518a6c3832b6a8007e0147a1bd0e0bda94c002
rickchang <rickchang@rickchangs-MacBook-Pro.local>
2021-03-26 00:13:22 +0800

    feat() add data spot on map

--- src/container/TaiwanMap.jsx
+++ src/container/TaiwanMap.jsx
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import ReactMapGL, { Marker } from 'react-map-gl'
-import { getSecondItemFromArray } from '../utils'
+import { groupBy } from 'lodash'
+import { getSecondItemFromArray, countby } from '../utils'
 import axios from 'axios'
 
+// 打點
+// https://docs.mapbox.com/mapbox-gl-js/example/filter-features-within-map-view/
+// https://docs.mapbox.com/mapbox-gl-js/example/data-driven-circle-colors/
+// 標區
+// https://docs.mapbox.com/mapbox-gl-js/example/data-join/
+// 標區＋legend
+// https://docs.mapbox.com/mapbox-gl-js/example/updating-choropleth/
 const stateMapping = {
   100: '準備啟用中',
   1: '啟用中',
   98: '維修中',
   99: '測試中',
 }
+
+const StaionMarker = React.memo(({arr}) => {
+  console.log('MARKER RENDER')
+  console.log(arr)
+  return arr.map(station => (
+    <Marker
+      key={station.rid}
+      latitude={station.latitude}
+      longitude={station.longitude}
+    >
+      <img style={{zIndex: 999, width:30, height:30}} src='station.svg'/>
+    </Marker>
+  ))
+})
 
 const TaiwanMap = () => {
   const [viewport, setViewport] = useState({
@@ -19,29 +41,51 @@ const TaiwanMap = () => {
     zoom: 8
   })
   const [stationData, setStationData] = useState([])
+  const [statistic, setStatistic] = useState([])
   useEffect(() => {
     fetchData()
   }, [])
 
   const fetchData = async () => {
     let result = []
+    let aggrData = {}
     try {
       const res = await axios.get('https://webapi.gogoro.com/api/vm/list')
-      const { data } = res
-      result = parseStationData(data)
+      const twJson = await axios.get('taiwan_density.json')
+      const { data: cityData } = twJson
+      const { data: stationData } = res
+      // log data
+      result = parseStationData(stationData)
+      // aggr data
+      const resultInGroup = countby(result, 'city_en')
+
+      aggrData = cityData.map(dt => {
+        if(!resultInGroup[dt.city]) return {}
+        const {
+          count: station_count,
+          ratio: station_ratio
+        } = resultInGroup[dt.city][0]
+        console.log(resultInGroup[dt.city][0])
+        return {
+          ...dt,
+          station_density: station_count ? (station_count / dt.area) : 0,
+          station_count,
+          station_ratio
+        }
+      })
     } catch (err) {
       console.log(err)
       alert('Something Went Wrong')
     }
-    console.log(result)
     setStationData(result)
+    setStatistic(aggrData)
   }
 
   const parseStationData = (arr) => {
     return arr.map(dt => {
       const address = getSecondItemFromArray(JSON.parse(dt.Address)['List'])['Value']
       const city = getSecondItemFromArray(JSON.parse(dt.City)['List'])['Value']
-      const district = getSecondItemFromArray(JSON.parse(dt.District)['List'])['Value'] 
+      const district = getSecondItemFromArray(JSON.parse(dt.District)['List'])['Value']
       const locName = getSecondItemFromArray(JSON.parse(dt.LocName)['List'])['Value']
       return {
         id: dt.Id,
@@ -50,6 +94,7 @@ const TaiwanMap = () => {
         zipCode: dt.ZipCode,
         address,
         city,
+        city_en: JSON.parse(dt.City)['List'][0]['Value'],
         district,
         locName,
         stateCode: dt.State,
@@ -58,27 +103,18 @@ const TaiwanMap = () => {
     }).filter(dt => dt.state === '啟用中')
   }
 
-  
+
 
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken='pk.eyJ1IjoieXVuaW5nYzAzMjUiLCJhIjoiY2ttbm96MGs0MDQ5NzJubWdrNnZkM2pvcyJ9.m5pyg4oP7RiLkDkOLYJW0w'
-      // mapStyle='mapbox://styles/yuningc0325/ckmnpe5yalnt817tfwbuf5yeu?optimize=true'
+      mapStyle='mapbox://styles/yuningc0325/ckmnpe5yalnt817tfwbuf5yeu?optimize=true'
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
-      {stationData.length > 0 ? stationData.map(station => (
-        <Marker
-          key={station.rid}
-          latitude={station.latitude}
-          longitude={station.longitude}
-        >
-          <img style={{zIndex: 999, width:10, height:10}} src='station.svg'/>
-        </Marker>
-      )): <></>}
-
+      {stationData.length > 0 ? <StaionMarker arr={stationData}/> : <></>}
     </ReactMapGL>  
-      
+
     // <div>{stationData.map(o => (<div>{o.locName}</div>))}</div>
   )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,21 @@
-import head from 'lodash/head'
+import { head, countBy, groupBy } from 'lodash'
 
 export const sum = (a, b) => a+b
 
 export const getSecondItemFromArray = (arr=[]) => {
   if(arr.length === 0) return null
   return head(arr.slice().reverse())
+}
+
+export const countby = (arr=[], by='id') => {
+  if(arr.length === 0) return []
+  const countGroup = countBy(arr, by)
+  const keys = Object.keys(countGroup)
+  const sum = Object.values(countGroup).reduce((pre, cur) => pre + cur, 0)
+  const result = keys.map(dt => ({
+    [by]: dt,
+    count: countGroup[dt],
+    ratio: parseFloat((countGroup[dt]/sum).toFixed(3))
+  }))
+  return groupBy(result, by)
 }
